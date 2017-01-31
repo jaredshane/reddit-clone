5c94a939142de716fbe15eb39f747f5532b47670
Jared Fuller <jaredshane@gmail.com>
2017-01-31 14:52:14 -0600

    added app.config

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
 console.log('hey')
 
 const app = angular.module('redditApp', ['ngRoute'])
+
+app.config(($routeProvider, $locationProvider) => {
+  $locationProvider.hashPrefix('')
+  $routeProvider
+    .when('/', {
+      controller: 'MainCtrl',
+      templateUrl: 'partials/main.html'
+    })
+
+})
