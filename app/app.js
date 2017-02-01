fc35ef48f8f6ab6cf02455f28178c240bc5bf7e0
Ben Conner <ben.conner40@gmail.com>
2017-02-01 15:01:04 -0600

    Initialize Firebase

--- app/app.js
+++ app/app.js
@@ -1,6 +1,13 @@
-console.log('hey')
-
 const app = angular.module('redditApp', ['ngRoute'])
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyBM8Q6ImvtSTC8BDg2EkXNdA5eR-03nItw",
+  authDomain: "reddit-clone-b97a6.firebaseapp.com",
+  databaseURL: "https://reddit-clone-b97a6.firebaseio.com",
+  storageBucket: "reddit-clone-b97a6.appspot.com",
+  messagingSenderId: "515019203543"
+});
 
 app.config(($routeProvider, $locationProvider) => {
   $locationProvider.hashPrefix('')
