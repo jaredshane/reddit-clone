d05134797d1d730d59c97881669a9c4a4c5b0dcf
Ben Conner <ben.conner40@gmail.com>
2017-02-03 10:58:25 -0600

    re-working how the html filters

--- app/controllers/mainctrl.js
+++ app/controllers/mainctrl.js
@@ -28,17 +28,19 @@ app.controller('MainCtrl', function ($http, $scope, $location) {
     console.log("filelist[0]", fileList[0])
     storageRef.child("images/" + fileList[0].name).put(fileList[0])
         .then(function(snapshot) {
-              console.log('Uploaded a file!')
-              console.log("fileList", fileList);
-                storageRef.child(`images/${fileList[0].name}`).getDownloadURL()
-                .then(function(url) {
-                var img = document.getElementById('myImg')
-                img.src = url;
-                console.log(url)
-            }).catch(function (error){
+            console.log('Uploaded a file!')
+            console.log("fileList", fileList);
+            storageRef.child(`images/${fileList[0].name}`).getDownloadURL()
+            .then(function(url) {
+            var img = document.getElementById('myImg')
+            img.src = url;
+            console.log(url)
             })
-        }
-   )} //end of handleFiles
+            .catch(function (error){
+                console.log("error");
+            })
+        })
+  } //end of handleFiles
 
    $scope.saveFirebase = (title,checkbox) => {
 
